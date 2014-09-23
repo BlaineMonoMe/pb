2190f8a67d72b7bbd2abc6f2bdca6f1ef41e6e65
BlaineMonoMe <fallower@ukr.net>
2014-09-23 10:57:27 +0300

    TableMessagesParser stub is ready

---
+++ src/main/java/org/pb/inputMessagesAnalyzer/TableMessagesParser.java
@@ -0,0 +1,29 @@
+package org.pb.inputMessagesAnalyzer;
+
+/**
+ * As a field-member Decision Making subsystem class (reference on it) must be
+ * present.
+ * 
+ * @author Yuriy
+ * 
+ */
+public class TableMessagesParser {
+
+	/**
+	 * this method is called from Input subsystem, or SystemManager class
+	 * 
+	 * @param inputMsg
+	 */
+	public void takeNewMessage(String inputMsg) {
+
+	}
+
+	/**
+	 * sends message to Decision Making subsystem class (reference on it)
+	 * 
+	 * @param outputMsg
+	 */
+	public void sendNewMessage(String outputMsg) {
+
+	}
+}
