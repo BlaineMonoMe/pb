afa4712ddce5cb603b4ba4c192c405ce6d1fc622
aros <aros08@yandex.ru>
2014-10-24 15:53:19 +0300

    hand parser added

--- src/main/java/org/pb/state/Card.java
+++ src/main/java/org/pb/state/Card.java
@@ -1,14 +1,56 @@
 package org.pb.state;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Card {
+
+    private static Map<String, Integer> cardSymbolicValueToIntMap = new LinkedHashMap<String, Integer>() {
+        {
+            put("2", 2);
+            put("3", 3);
+            put("4", 4);
+            put("5", 5);
+            put("6", 6);
+            put("7", 7);
+            put("8", 8);
+            put("9", 9);
+            put("T", 10);
+            put("J", 11);
+            put("Q", 12);
+            put("K", 13);
+            put("A", 14);
+        }
+    };
+
+    private static Map<Integer, String> cardIntValueToSymbolicMap = new LinkedHashMap<Integer, String>() {
+        {
+            put(2, "2");
+            put(3, "3");
+            put(4, "4");
+            put(5, "5");
+            put(6, "6");
+            put(7, "7");
+            put(8, "8");
+            put(9, "9");
+            put(10, "T");
+            put(11, "J");
+            put(12, "Q");
+            put(13, "K");
+            put(14, "A");
+        }
+    };
+
 	// mast'
 	private char suit;
 	// from 2 to Ace
 	private int rank;
+    private String rankStringValue;
 
 	public Card(Card card) {
 		this.suit = card.getSuit();
 		this.rank = card.getRank();
+        rankStringValue = cardIntValueToSymbolicMap.get(rank);
 	}
 
 	public Card(char suit, int rank) {
@@ -18,32 +60,9 @@ public class Card {
 
 	public Card(char suit, char rank) {
 		this.suit = suit;
-		if (rank == '2')
-			this.rank = 2;
-		if (rank == '3')
-			this.rank = 3;
-		if (rank == '4')
-			this.rank = 4;
-		if (rank == '5')
-			this.rank = 5;
-		if (rank == '6')
-			this.rank = 6;
-		if (rank == '7')
-			this.rank = 7;
-		if (rank == '8')
-			this.rank = 8;
-		if (rank == '9')
-			this.rank = 9;
-		if (rank == 'T')
-			this.rank = 10;
-		if (rank == 'J')
-			this.rank = 11;
-		if (rank == 'Q')
-			this.rank = 12;
-		if (rank == 'K')
-			this.rank = 13;
-		if (rank == 'A')
-			this.rank = 14;
+        rankStringValue = String.valueOf(rank);
+		this.rank = cardSymbolicValueToIntMap.get(rankStringValue);
+
 	}
 
 	public Card() {
@@ -69,4 +88,12 @@ public class Card {
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
+
+    public String getRankStringValue() {
+        return rankStringValue;
+    }
+
+    public void setRankStringValue(String rankStringValue) {
+        this.rankStringValue = rankStringValue;
+    }
 }
